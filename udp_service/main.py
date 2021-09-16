326b1e7d3bca08b192d05b48a498ff1f23aacf36
egor-lukyanov <elukianov@isgneuro.com>
2021-09-16 19:11:19 +0300

    feat: multiprocessing

--- udp_service/main.py
+++ udp_service/main.py
@@ -1,4 +1,5 @@
 from time import sleep
+from multiprocessing import Process
 
 from data_parser import BasicParser
 from data_reader import DataReader
@@ -7,22 +8,11 @@ from logger import logger
 from config import import_config
 
 
-def main(config_file):
-    conf = import_config(config_file)
-    logger.set_logger(
-        **conf.get('logging')
-    )
-    logger.log("Logger inited.")
+def process_packets(conf, packets):
     parser = BasicParser(
         logger,
         sensors=conf.get('sensors'),
         pattern=conf.get('pattern')
-    )
-    reader = DataReader(
-        logger,
-        base_dir=conf.get('paths', {}).get('rawpath'),
-        file_size=1000000,
-        delete_files=True
     )
     writer = DataWriter(
         logger,
@@ -30,13 +20,37 @@ def main(config_file):
         batch_length=10000
     )
 
+    for packet in packets:
+        data = parser.parse_packet(packet)
+        writer.write_line(data)
+
+
+def main(config_file):
+    conf = import_config(config_file)
+    logger.set_logger(
+        **conf.get('logging')
+    )
+    logger.log("Logger inited.")
+
+    reader = DataReader(
+        logger,
+        base_dir=conf.get('paths', {}).get('rawpath'),
+        file_size=1000000,
+        delete_files=True
+    )
+
     while 1:
-        packets = reader.read_data()
-
-        for packet in packets:
-            data = parser.parse_packet(packet)
-            writer.write_line(data)
-
+        if conf.get('cores', 0) > 1:
+            procs = []
+            for _ in range(conf.get('cores', 1)):
+                packets = reader.read_data()
+                p = Process(target=process_packets, args=(conf, packets))
+                p.start()
+                procs.append(p)
+            for proc in procs:
+                proc.join()
+        else:
+            process_packets(conf, reader.read_data())
         sleep(0.0001)
 
 
